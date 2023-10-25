c264dbe5b8335bbb6f682ec46ff416271195aae4
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-10-24 21:25:20 -0400

    Test and implement Position class

---
+++ app/src/main/java/edu/ramapo/bbhatta/cmps366_pente_java/Position.java
@@ -0,0 +1,157 @@
+package edu.ramapo.bbhatta.cmps366_pente_java;
+
+/**
+ * @class Position
+ * @brief This class is used to store the row and column of a position on the board.
+ */
+public class Position {
+    /**
+     * The row of the position.
+     */
+    protected int _row;
+    /**
+     * The column of the position.
+     */
+    protected int _col;
+
+
+    /**
+     * Default constructor for the Position class.
+     *
+     * @param row The row of the position.
+     * @param col The column of the position.
+     */
+    public Position(int row, int col) {
+        this._row = row;
+        this._col = col;
+    }
+
+    /**
+     * Copy constructor for the Position class.
+     *
+     * @param position The position to copy.
+     */
+    public Position(Position position) {
+        this._row = position.row();
+        this._col = position.col();
+    }
+
+    /**
+     * Gets the row of the position.
+     *
+     * @return The row of the position.
+     */
+    public int row() {
+        return _row;
+    }
+
+    /**
+     * Gets the column of the position.
+     *
+     * @return The column of the position.
+     */
+    public int col() {
+        return _col;
+    }
+
+    /**
+     * Checks if the position is equal to another position.
+     *
+     * @param position
+     * @return True if the positions are equal, false otherwise.
+     */
+    public boolean equals(Position position) {
+        return this._row == position.row() && this._col == position.col();
+    }
+
+    /**
+     * Gets the position above this position.
+     *
+     * @return The position above this position.
+     */
+    public Position up() {
+        return new Position(_row - 1, _col);
+    }
+
+    /**
+     * Gets the position below this position.
+     *
+     * @return The position below this position.
+     */
+    public Position down() {
+        return new Position(_row + 1, _col);
+    }
+
+    /**
+     * Gets the position to the left of this position.
+     *
+     * @return The position to the left of this position.
+     */
+    public Position left() {
+        return new Position(_row, _col - 1);
+    }
+
+    /**
+     * Gets the position to the right of this position.
+     *
+     * @return The position to the right of this position.
+     */
+    public Position right() {
+        return new Position(_row, _col + 1);
+    }
+
+    /**
+     * Gets the position above and to the left of this position.
+     *
+     * @return The position above and to the left of this position.
+     */
+    public Position upLeft() {
+        return new Position(_row - 1, _col - 1);
+    }
+
+    /**
+     * Gets the position above and to the right of this position.
+     *
+     * @return The position above and to the right of this position.
+     */
+    public Position upRight() {
+        return new Position(_row - 1, _col + 1);
+    }
+
+    /**
+     * Gets the position below and to the left of this position.
+     *
+     * @return The position below and to the left of this position.
+     */
+    public Position downLeft() {
+        return new Position(_row + 1, _col - 1);
+    }
+
+    /**
+     * Gets the position below and to the right of this position.
+     *
+     * @return The position below and to the right of this position.
+     */
+    public Position downRight() {
+        return new Position(_row + 1, _col + 1);
+    }
+
+
+
+    /**
+     * Converts the position to a string.
+     *
+     * @param noRowsInBoard The number of rows in the board.
+     *                      This is used to convert the row to a letter.
+     *                      The letter is the (noRowsInBoard - row + 1) converted to a letter.
+     *                      For example, if noRowsInBoard is 19 and the row is 0, then the letter is 'S'.
+     *                      If noRowsInBoard is 19 and the row is 18, then the letter is 'A'.
+     *                      The number of columns is always a number, so the total number of columns
+     *                      is not required.
+     * @return The position as a string.
+     */
+    public String toString(int noRowsInBoard) {
+        char row = (char) ('A' + (noRowsInBoard - _row - 1));
+        return String.format("%c%d", row, _col + 1);
+    }
+}
---
+++ app/src/test/java/edu/ramapo/bbhatta/cmps366_pente_java/PositionTest.java
@@ -0,0 +1,121 @@
+package edu.ramapo.bbhatta.cmps366_pente_java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PositionTest {
+    @Test
+    public void testConstructor() {
+        Position position = new Position(1, 2);
+        assertEquals(1, position.row());
+        assertEquals(2, position.col());
+    }
+
+    @Test
+    public void testCopyConstructor() {
+        Position position1 = new Position(1, 2);
+        Position position2 = new Position(position1);
+        assertEquals(position1.row(), position2.row());
+        assertEquals(position1.col(), position2.col());
+    }
+
+    @Test
+    public void testEquals() {
+        Position position1 = new Position(1, 2);
+        Position position2 = new Position(1, 2);
+        Position position3 = new Position(2, 3);
+        assertTrue(position1.equals(position2));
+        assertFalse(position1.equals(position3));
+    }
+
+    @Test
+    public void testRow() {
+        Position position = new Position(1, 2);
+        assertEquals(1, position.row());
+    }
+
+    @Test
+    public void testCol() {
+        Position position = new Position(1, 2);
+        assertEquals(2, position.col());
+    }
+
+    @Test
+    public void testUp() {
+        Position position = new Position(2, 3);
+        Position up = position.up();
+        assertEquals(1, up.row());
+        assertEquals(3, up.col());
+    }
+
+    @Test
+    public void testDown() {
+        Position position = new Position(2, 3);
+        Position down = position.down();
+        assertEquals(3, down.row());
+        assertEquals(3, down.col());
+    }
+
+    @Test
+    public void testLeft() {
+        Position position = new Position(2, 3);
+        Position left = position.left();
+        assertEquals(2, left.row());
+        assertEquals(2, left.col());
+    }
+
+    @Test
+    public void testRight() {
+        Position position = new Position(2, 3);
+        Position right = position.right();
+        assertEquals(2, right.row());
+        assertEquals(4, right.col());
+    }
+
+    @Test
+    public void testUpLeft() {
+        Position position = new Position(2, 3);
+        Position upLeft = position.upLeft();
+        assertEquals(1, upLeft.row());
+        assertEquals(2, upLeft.col());
+    }
+
+    @Test
+    public void testUpRight() {
+        Position position = new Position(2, 3);
+        Position upRight = position.upRight();
+        assertEquals(1, upRight.row());
+        assertEquals(4, upRight.col());
+    }
+
+    @Test
+    public void testDownLeft() {
+        Position position = new Position(2, 3);
+        Position downLeft = position.downLeft();
+        assertEquals(3, downLeft.row());
+        assertEquals(2, downLeft.col());
+    }
+
+    @Test
+    public void testDownRight() {
+        Position position = new Position(2, 3);
+        Position downRight = position.downRight();
+        assertEquals(3, downRight.row());
+        assertEquals(4, downRight.col());
+    }
+
+    @Test
+    public void testToStringTopRow() {
+        Position position = new Position(0, 0);
+        String str = position.toString(19);
+        assertEquals("S1", str);
+    }
+
+    @Test
+    public void testToStringBottomRow() {
+        Position position = new Position(18, 18);
+        String str = position.toString(19);
+        assertEquals("A19", str);
+    }
+}
