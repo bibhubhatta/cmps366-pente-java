9e15e6bea7185ddc87fcac32ef630da06593e72d
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-11-01 15:12:01 -0400

    Split tests to test only one behaviour

--- app/src/test/java/edu/ramapo/bbhatta/cmps366_pente_java/DemoTest.java
+++ app/src/test/java/edu/ramapo/bbhatta/cmps366_pente_java/DemoTest.java
@@ -7,9 +7,6 @@ import java.io.IOException;
 
 import static edu.ramapo.bbhatta.cmps366_pente_java.Player.COMPUTER;
 import static edu.ramapo.bbhatta.cmps366_pente_java.Player.HUMAN;
-import static org.hamcrest.CoreMatchers.anyOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 
 public class DemoTest {
@@ -18,46 +15,39 @@ public class DemoTest {
 
 
     @Test
-    public void testSerial1() {
+    public void testInitialCaptures() {
+        Pente pente = getPenteFromFile();
+        assertEquals(0, pente.getCaptures(HUMAN));
+        assertEquals(0, pente.getCaptures(COMPUTER));
+    }
 
-        Pente pente;
+    @Test
+    public void testInitialRoundScore() {
+        Pente pente = getPenteFromFile();
+        assertEquals(0, pente.getRoundScore(HUMAN));
+        assertEquals(0, pente.getRoundScore(COMPUTER));
+    }
+
+    @Test
+    public void testCurrentPlayerAndStone() {
+        Pente pente = getPenteFromFile();
+        assertEquals(HUMAN, pente.getCurrentPlayer());
+        assertEquals(Stone.BLACK, pente.getCurrentStone());
+    }
+
+    @Test
+    public void testMakeMove() {
+        Pente pente = getPenteFromFile();
+        pente = pente.makeMove("J12");
+        assertEquals(2, pente.getNoCaptures(HUMAN));
+        assertEquals(0, pente.getNoCaptures(COMPUTER));
+    }
+
+    private Pente getPenteFromFile() {
         try {
-            pente = Pente.fromFile(serial1File);
+            return Pente.fromFile(serial1File);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        assert pente != null;
-        assertEquals(0, pente.getCaptures(HUMAN));
-        assertEquals(0, pente.getCaptures(HUMAN));
-
-        assertEquals(0, pente.getRoundScore(HUMAN));
-        assertEquals(0, pente.getRoundScore(COMPUTER));
-
-        assertEquals(HUMAN, pente.getCurrentPlayer());
-        assertEquals(Stone.BLACK, pente.getCurrentStone());
-
-        System.out.printf(pente.getBoard().displayString());
-
-//        assertEquals(pente.getBoard().stringToPosition("J12"), pente.getBestMove());
-
-        pente = pente.makeMove("J12");
-
-        System.out.printf(pente.getBoard().displayString());
-
-        assertEquals(2, pente.getNoCaptures(HUMAN));
-        assertEquals(0, pente.getNoCaptures(COMPUTER));
-
-        assertEquals(2, pente.getRoundScore(HUMAN));
-        assertEquals(0, pente.getRoundScore(COMPUTER));
-
-        assertEquals(COMPUTER, pente.getCurrentPlayer());
-        assertEquals(Stone.WHITE, pente.getCurrentStone());
-
-        assertThat(pente.getBestMove(), anyOf(
-                        is(pente.getBoard().stringToPosition("C2")),
-                        is(pente.getBoard().stringToPosition("C18"))
-                )
-        );
     }
 }
