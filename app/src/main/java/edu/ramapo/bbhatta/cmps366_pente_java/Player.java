468bf98bce1e0ab76368837e5af5893597df04f1
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-11-01 14:33:19 -0400

    Add player class

---
+++ app/src/main/java/edu/ramapo/bbhatta/cmps366_pente_java/Player.java
@@ -0,0 +1,43 @@
+package edu.ramapo.bbhatta.cmps366_pente_java;
+
+import java.util.Objects;
+
+/**
+ * Player class represents a player in the game.
+ * It is immutable.
+ * <p>
+ * The player is represented by a name.
+ * </p>
+ */
+public class Player {
+
+    public static final Player HUMAN = new Player("Human");
+    public static final Player COMPUTER = new Player("Computer");
+
+    protected String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public Player(Player player) {
+        this.name = player.name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Player)) return false;
+        Player player = (Player) o;
+        return Objects.equals(name, player.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
