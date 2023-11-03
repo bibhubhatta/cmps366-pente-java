d2a2b0b14c88877aad9069c7ffeb525f20c4add4
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-11-02 22:16:42 -0400

    Add TournamentActivity

--- app/src/main/java/edu/ramapo/bbhatta/cmps366_pente_java/MainActivity.java
+++ app/src/main/java/edu/ramapo/bbhatta/cmps366_pente_java/MainActivity.java
@@ -2,58 +2,44 @@ package edu.ramapo.bbhatta.cmps366_pente_java;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button startNewTournamentButton;
-    private Button loadTournamentButton;
+    public static final int BOARD_SIZE = 19;
+    public static Tournament tournament;
+
+    private static void initializeTournament() {
+
+        tournament = new Tournament(BOARD_SIZE, BOARD_SIZE);
+
+        tournament = tournament.addPlayer(Player.HUMAN);
+        tournament = tournament.addPlayer(Player.COMPUTER);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startNewTournamentButton = findViewById(R.id.btn_start_new_tournament);
-        loadTournamentButton = findViewById(R.id.btn_load_tournament);
+        Button startNewTournamentButton = findViewById(R.id.btn_start_new_tournament);
+        Button loadTournamentButton = findViewById(R.id.btn_load_tournament);
 
-        startNewTournamentButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Tournament newTournament = getNewTournament();
-            }
+        startNewTournamentButton.setOnClickListener(view -> {
+
+            initializeTournament();
+
+            Intent intent = new Intent(MainActivity.this, TournamentActivity.class);
+            startActivity(intent);
         });
 
-    }
+        loadTournamentButton.setOnClickListener(view -> {
+//            Intent intent = new Intent(MainActivity.this, LoadTournamentActivity.class);
+//            startActivity(intent);
+        });
 
-    private Tournament getNewTournament() {
-        boolean humanWinsToss = conductToss();
-
-        // Create a new game
-        Tournament newTournament = new Tournament(19, 19);
-        Player human = Player.HUMAN;
-        Player computer = Player.COMPUTER;
-
-        if (humanWinsToss) {
-            newTournament = newTournament.addPlayer(human);
-            newTournament = newTournament.addPlayer(computer);
-        } else {
-            newTournament = newTournament.addPlayer(computer);
-            newTournament = newTournament.addPlayer(human);
-        }
-        return newTournament;
-    }
-
-    private boolean conductToss() {
-        // Conduct a toss to see who goes first
-        // Start the CoinTossActivity Activity when the button is clicked
-        Intent intent = new Intent(MainActivity.this, CoinTossActivity.class);
-        startActivity(intent);
-
-        // Get the result of the toss from the CoinTossActivity Activity
-        return getIntent().getBooleanExtra(CoinTossActivity.COIN_TOSS_RESULT_KEY, false);
     }
 
 
---
+++ app/src/main/java/edu/ramapo/bbhatta/cmps366_pente_java/TournamentActivity.java
@@ -0,0 +1,78 @@
+package edu.ramapo.bbhatta.cmps366_pente_java;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+import static edu.ramapo.bbhatta.cmps366_pente_java.MainActivity.tournament;
+
+public class TournamentActivity extends AppCompatActivity {
+
+    private TextView playerScoresTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tournament);
+
+
+        playerScoresTextView = findViewById(R.id.playerScoresTextView);
+        Button endTournamentButton = findViewById(R.id.endTournamentButton);
+        Button playRoundButton = findViewById(R.id.playRoundButton);
+
+        displayPlayerScores();
+
+        endTournamentButton.setOnClickListener(view -> {
+            //  TODO: Show winner
+            finish();
+        });
+
+        playRoundButton.setOnClickListener(view -> {
+                    try {
+                        tournament = tournament.initializeRound();
+                    } catch (IllegalStateException e) {
+                        boolean humanGoesFirst = conductToss();
+
+                        ArrayList<Player> players = new ArrayList<>();
+                        if (humanGoesFirst) {
+                            players.add(Player.HUMAN);
+                            players.add(Player.COMPUTER);
+
+                        } else {
+                            players.add(Player.COMPUTER);
+                            players.add(Player.HUMAN);
+
+                        }
+                        tournament = tournament.initializeRound(players);
+                    }
+
+//                    Intent intent = new Intent(TournamentActivity.this, RoundActivity.class);
+//                    startActivity(intent);
+                }
+        );
+    }
+
+    private void displayPlayerScores() {
+        // Get the player scores from the Tournament instance and display them
+        StringBuilder playerScores = new StringBuilder("Player Scores:\n");
+        for (Player player : tournament.getPlayers()) {
+            playerScores.append(player.getName()).append(": ").append(tournament.getScore(player)).append("\n");
+        }
+        playerScoresTextView.setText(playerScores.toString());
+    }
+
+
+    private boolean conductToss() {
+        // Conduct a toss to see who goes first
+        // Start the CoinTossActivity Activity when the button is clicked
+        Intent intent = new Intent(TournamentActivity.this, CoinTossActivity.class);
+        startActivity(intent);
+
+        // Get the result of the toss from the CoinTossActivity Activity
+        return getIntent().getBooleanExtra(CoinTossActivity.COIN_TOSS_RESULT_KEY, false);
+    }
+}
