02ddff51c3f235e6ea4d35732a0985db0deb6f5c
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-11-06 20:42:19 -0500

    Add comments

--- app/src/main/java/edu/ramapo/bbhatta/cmps366_pente_java/Strategy.java
+++ app/src/main/java/edu/ramapo/bbhatta/cmps366_pente_java/Strategy.java
@@ -22,6 +22,11 @@ public class Strategy {
 
     /**
      * Get the best move
+     * <p>
+     * The best move is calculated by getting the pseudo score for each available move.
+     * The moves with the highest pseudo score are stored in a list and a move is chosen at random.
+     * <p>
+     * See getPseudoScore() for more information on how the pseudo score is calculated.
      *
      * @return The best move for the player.
      */
@@ -47,6 +52,25 @@ public class Strategy {
         return bestMoves.get(random.nextInt(bestMoves.size()));
     }
 
+    /**
+     * Calculates the pseudo score for a given move.
+     * <p>
+     * The pseudo score is calculated by making the move on a copy of the current round.
+     * The score is calculated by adding the following for each player:
+     * - The score of the player
+     * - The number of captures the player has
+     * - The score of the player's sequences
+     * The score is then subtracted by the distance of the move from the center of the board so that
+     * moves closer to the center are prioritized over edge moves.
+     * <p>
+     * The score is weighted based on the player so that offensive moves are prioritized over
+     * defensive moves.
+     * <p>
+     * See getSequenceScore() for more information on how the sequence score is calculated.
+     *
+     * @param position The move to calculate the pseudo score for.
+     * @return The pseudo score for the given move.
+     */
     private int getPseudoScore(Position position) {
         int score = 0;
 
@@ -71,6 +95,16 @@ public class Strategy {
         return score;
     }
 
+    /**
+     * Calculates the sequence score for a given position.
+     * <p>
+     * The sequence score is calculated by getting all the sequences on the board and adding the
+     * square of the length of the sequence if the sequence contains the stone at the given position.
+     *
+     * @param round    The round to calculate the sequence score for.
+     * @param position The position to calculate the sequence score for.
+     * @return The sequence score for the given position.
+     */
     private int getSequenceScore(Round round, Position position) {
         int score = 0;
 
