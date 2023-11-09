7d13b05411c442f2cb62c8e9a9050b21ce5ed5ab
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-11-08 19:23:42 -0500

    Refactor

--- app/src/main/java/edu/ramapo/bbhatta/cmps366_pente_java/SaveActivity.java
+++ app/src/main/java/edu/ramapo/bbhatta/cmps366_pente_java/SaveActivity.java
@@ -5,7 +5,6 @@ import android.content.ContentValues;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -22,34 +21,32 @@ public class SaveActivity extends Activity {
         final EditText fileNameEditText = findViewById(R.id.fileNameEditText);
         Button saveButton = findViewById(R.id.saveButton);
 
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String fileName = fileNameEditText.getText().toString();
-                saveTextToDownloadsDirectory(fileName);
-            }
+        saveButton.setOnClickListener(view -> {
+            String fileName = fileNameEditText.getText().toString();
+            saveTextToDownloadsDirectory(fileName);
         });
     }
 
     private void saveTextToDownloadsDirectory(String fileName) {
         ContentValues values = new ContentValues();
-        values.put(MediaStore.Downloads.DISPLAY_NAME, fileName);
-        values.put(MediaStore.Downloads.MIME_TYPE, "text/plain");
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName);
+        values.put(MediaStore.MediaColumns.MIME_TYPE, "text/plain");
 
         Uri uri = getContentResolver().insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values);
+        if (uri == null) {
+            Toast.makeText(this, "Failed to save file", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        try {
-            if (uri != null) {
-                OutputStream outputStream = getContentResolver().openOutputStream(uri);
-                if (outputStream != null) {
-                    outputStream.write("hello".getBytes());
-                    outputStream.close();
-                    Toast.makeText(this, "Text saved to Downloads", Toast.LENGTH_SHORT).show();
-                }
-            }
+
+        try (OutputStream outputStream = getContentResolver().openOutputStream(uri)) {
+            outputStream.write("hello".getBytes());
+            Toast.makeText(this, "Text saved to Downloads", Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
             e.printStackTrace();
             Toast.makeText(this, "Failed to save text", Toast.LENGTH_SHORT).show();
         }
+
     }
 }
+
